990219700783c42537dd395d3debb4b5eb60eb4a
Daniel Leonardo <daniel.fnz@hotmail.com>
2019-02-22 21:31:18 -0300

    Inicializando firebase database

--- app/src/main/java/danielfnz/com/br/androidfirebase/MainActivity.java
+++ app/src/main/java/danielfnz/com/br/androidfirebase/MainActivity.java
@@ -3,11 +3,25 @@ package danielfnz.com.br.androidfirebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.inicializarFirebase();
     }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
 }
